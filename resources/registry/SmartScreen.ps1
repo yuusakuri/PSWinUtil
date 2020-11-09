289d78e587fbd4f9e8957541fa4fc1dcaf28d1ff
YusakuMori <chestnut1623@gmail.com>
2020-11-09 11:33:00 +0900

    SmartScreenを有効・無効にする関数を追加

---
+++ resources/registry/SmartScreen.ps1
@@ -0,0 +1,14 @@
+﻿@{
+  SmartScreen = @{
+    Machine = @{
+      KeyName   = 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System
+      '
+      ValueName = 'EnableSmartScreen'
+      Type      = 'REG_DWORD'
+      Data      = @{
+        Enable  = 1
+        Disable = 0
+      }
+    }
+  }
+}
