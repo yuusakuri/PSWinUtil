9d8424d273c6fe48c7fa8c34a7137dbaceb8727f
yuusakuri <yuusakurip1@gmail.com>
2020-11-14 21:24:02 +0900

    機能させるには手順が足りていなかったためスクリプトを追加

--- functions/registry/Disable-WUSystemSounds.ps1
+++ functions/registry/Disable-WUSystemSounds.ps1
@@ -14,3 +14,12 @@ if (!$registryHash) {
 }
 
 Set-WURegistryFromHash -RegistryHash $registryHash -DataKey $MyInvocation.MyCommand.Verb
+
+$value = ""
+Get-ChildItem -Path "HKCU:\AppEvents\Schemes\Apps" |
+Get-ChildItem |
+ForEach-Object {
+  Get-ChildItem  $_.PSPath | Where-Object { $_.PSChildName -eq ".Current" } |
+  Get-ItemProperty | Where-Object { $_ | Get-Member -Name "(Default)" } |
+  Set-ItemProperty -Name "(Default)" -Value $value
+}
--- functions/registry/Enable-WUSystemSounds.ps1
+++ functions/registry/Enable-WUSystemSounds.ps1
@@ -14,3 +14,15 @@ if (!$registryHash) {
 }
 
 Set-WURegistryFromHash -RegistryHash $registryHash -DataKey $MyInvocation.MyCommand.Verb
+
+Get-ChildItem -Path "HKCU:\AppEvents\Schemes\Apps" |
+Get-ChildItem |
+ForEach-Object {
+  $value = Get-ChildItem $_.PSPath | Where-Object { $_.PSChildName -eq ".default" } |
+  Get-ItemProperty | Where-Object { $_ | Get-Member -Name "(Default)" } |
+  Get-ItemPropertyValue -Name "(Default)"
+
+  Get-ChildItem  $_.PSPath | Where-Object { $_.PSChildName -eq ".Current" } |
+  Get-ItemProperty | Where-Object { $_ | Get-Member -Name "(Default)" } |
+  Set-ItemProperty -Name "(Default)" -Value $value
+}
