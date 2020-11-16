d9e5879e68de557c36ef74b707334d3864f98f4f
yuusakuri <yuusakurip1@gmail.com>
2020-11-16 22:24:37 +0900

    Windows key + L でロックされないようにするためのスクリプトを追加

---
+++ resources/registry/LockWorkstation.ps1
@@ -0,0 +1,13 @@
+﻿@{
+  LockWorkstation = @{
+    User = @{
+      KeyName   = 'HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System'
+      ValueName = 'DisableLockWorkstation'
+      Type      = 'REG_DWORD'
+      Data      = @{
+        Enable  = 0
+        Disable = 1
+      }
+    }
+  }
+}
