06494065d1871cc4b1c4a9503e63011a1e89cadc
yuusakuri <yuusakurip1@gmail.com>
2020-11-18 21:50:54 +0900

    リファクタリング

--- functions/registry/Disable-WUSystemSounds.ps1
+++ functions/registry/Disable-WUSystemSounds.ps1
@@ -19,7 +19,7 @@ $value = ""
 Get-ChildItem -Path "HKCU:\AppEvents\Schemes\Apps" |
 Get-ChildItem |
 ForEach-Object {
-  Get-ChildItem  $_.PSPath | Where-Object { $_.PSChildName -eq ".Current" } |
+  Get-ChildItem $_.PSPath | Where-Object { $_.PSChildName -eq ".Current" } |
   Get-ItemProperty | Where-Object { $_ | Get-Member -Name "(Default)" } |
   Set-ItemProperty -Name "(Default)" -Value $value
 }
