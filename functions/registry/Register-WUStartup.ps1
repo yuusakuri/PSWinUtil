46acf88707897861dc977e9d8a668047658d52d9
yuusakuri <yuusakurip1@gmail.com>
2020-12-03 03:05:34 +0900

    特定のvalue dataを空（ダブルクォーテーション2つ）にするオプションを追加
    valueを残したままにしておきたい場合のため

--- functions/registry/Register-WUStartup.ps1
+++ functions/registry/Register-WUStartup.ps1
@@ -14,72 +14,87 @@
 #>
 
 [CmdletBinding(
-  SupportsShouldProcess,
-  DefaultParameterSetName = 'Path'
+    SupportsShouldProcess,
+    DefaultParameterSetName = 'Path'
 )]
 param (
-  # Specify the location of the file to be registered in the startup. Wildcards are permitted.
-  [Parameter(Mandatory,
-    Position = 0,
-    ParameterSetName = 'Path',
-    ValueFromPipeline,
-    ValueFromPipelineByPropertyName)]
-  [ValidateNotNullOrEmpty()]
-  [SupportsWildcards()]
-  [string]
-  $Path,
+    # Specify the location of the file to be registered in the startup. Wildcards are permitted.
+    [Parameter(Mandatory,
+        Position = 0,
+        ParameterSetName = 'Path',
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [SupportsWildcards()]
+    [string]
+    $Path,
 
-  # Specify the location of the file to be registered in the startup. Unlike the Path parameter, the value of the LiteralPath parameter is used exactly as it is typed. No characters are interpreted as wildcards. If the path includes escape characters, enclose it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any characters as escape sequences.
-  [Parameter(Mandatory,
-    Position = 0,
-    ParameterSetName = 'LiteralPath',
-    ValueFromPipelineByPropertyName)]
-  [Alias('PSPath')]
-  [ValidateNotNullOrEmpty()]
-  [string]
-  $LiteralPath,
+    # Specify the location of the file to be registered in the startup. Unlike the Path parameter, the value of the LiteralPath parameter is used exactly as it is typed. No characters are interpreted as wildcards. If the path includes escape characters, enclose it in single quotation marks. Single quotation marks tell Windows PowerShell not to interpret any characters as escape sequences.
+    [Parameter(Mandatory,
+        Position = 0,
+        ParameterSetName = 'LiteralPath',
+        ValueFromPipelineByPropertyName)]
+    [Alias('PSPath')]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $LiteralPath,
 
-  # Specify the Value name of the registry to register. If nothing is specified, this value will be the file base name.
-  [string]
-  $Valuename,
+    # Specify this parameter if you want the value to be empty.
+    [Parameter(Position = 0,
+        ParameterSetName = 'Empty')]
+    [switch]
+    $Empty,
 
-  # Specify the argument to be passed to the file at startup.
-  [string]
-  $Argument,
+    # Specify the Value name of the registry to register. If nothing is specified, this value will be the file base name.
+    [string]
+    $Valuename,
 
-  # Specifies the scope that is affected. The default scope is CurrentUser.
-  [ValidateSet('LocalMachine', 'CurrentUser')]
-  [string]
-  $Scope = 'CurrentUser'
+    # Specify the argument to be passed to the file at startup.
+    [Parameter(ParameterSetName = 'Path')]
+    [Parameter(ParameterSetName = 'LiteralPath')]
+    [string]
+    $Argument,
+
+    # Specifies the scope that is affected. The default scope is CurrentUser.
+    [ValidateSet('LocalMachine', 'CurrentUser')]
+    [string]
+    $Scope = 'CurrentUser'
 )
 
 Set-StrictMode -Version 'Latest'
 $registryHash = Get-WURegistryHash
 if (!$registryHash) {
-  return
+    return
 }
 
 $paths = @()
-if ($psCmdlet.ParameterSetName -eq 'Path') {
-  $paths += Resolve-WUFullPath -Path $Path
+if ($psCmdlet.ParameterSetName -eq 'Empty') {
+    $paths += ""
+}
+elseif ($psCmdlet.ParameterSetName -eq 'Path') {
+    $paths += Resolve-WUFullPath -Path $Path
 }
 else {
-  $paths += Resolve-WUFullPath -LiteralPath $LiteralPath
+    $paths += Resolve-WUFullPath -LiteralPath $LiteralPath
 }
 
-if (!$paths -or $paths.Count -ne 1) {
-  Write-Error 'The Path must specify a single location.'
-  return
+if ($paths.Count -ne 1) {
+    Write-Error 'The Path must specify a single location.'
+    return
 }
 
 foreach ($aPath in $paths) {
-  if ($Valuename) {
-    $registryHash.Startup.$Scope.ValueName = $ValueName
-  }
-  else {
-    $registryHash.Startup.$Scope.ValueName = Split-Path $aPath -Leaf
-  }
-  $registryHash.Startup.$Scope.Data = '"{0}" {1}' -f $aPath, $Argument
+    if ($Valuename) {
+        $registryHash.Startup.$Scope.ValueName = $ValueName
+    }
+    else {
+        $registryHash.Startup.$Scope.ValueName = Split-Path $aPath -Leaf
+    }
 
-  Set-WURegistryFromHash -RegistryHash $registryHash -Scope $Scope
+    $registryHash.Startup.$Scope.Data = '"{0}"' -f $aPath
+    if ($Argument) {
+        $registryHash.Startup.$Scope.Data = '{0} {1}' -f $registryHash.Startup.$Scope.Data, $Argument
+    }
+
+    Set-WURegistryFromHash -RegistryHash $registryHash -Scope $Scope
 }
