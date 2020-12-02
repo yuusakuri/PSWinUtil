f50628aa204fe37f16b2a45a680824fb94ce9749
yuusakuri <yuusakurip1@gmail.com>
2020-12-03 07:14:02 +0900

    パスを除外する方法を変更

--- functions/Find-WUPath.ps1
+++ functions/Find-WUPath.ps1
@@ -38,162 +38,165 @@
 
 [CmdletBinding()]
 param (
-  # Specify the character string contained in the leaf of the path to be searched. In addition, the strings contained in its parent directory can be specified before the leaf, separated by / or \. However, if -Strict is specified, the path with exact leaf matches will be searched. Also, wildcards are not supported.
-  [Parameter(Mandatory,
-    Position = 0,
-    ValueFromPipeline,
-    ValueFromPipelineByPropertyName)]
-  [ValidateNotNullOrEmpty()]
-  [string[]]
-  $Name,
+    # Specify the character string contained in the leaf of the path to be searched. In addition, the strings contained in its parent directory can be specified before the leaf, separated by / or \. However, if -Strict is specified, the path with exact leaf matches will be searched. Also, wildcards are not supported.
+    [Parameter(Mandatory,
+        Position = 0,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [string[]]
+    $Name,
 
-  # Excludes paths that include the specified string. The following paths are excluded by default.
-  <#
+    # Excludes paths that contain the specified string. It is case sensitive. Paths containing the following strings are excluded regardless of the value specified for this parameter.
+
+    <#
     'C:\Windows\SysWOW64'
-    'C:\Windows\WinSxS'
+    'SxS'
     "$env:LOCALAPPDATA\Microsoft\Windows\FileHistory"
-  #>
-  [string[]]
-  $Exclude = @(
-    'C:\Windows\SysWOW64'
-    'C:\Windows\WinSxS'
-    "$env:LOCALAPPDATA\Microsoft\Windows\FileHistory"
-  ),
+    #>
+    [string[]]
+    $Exclude,
 
-  # Search for a path that has an exact leaf match.
-  [switch]
-  $Strict,
+    # Search for a path that has an exact leaf match.
+    [switch]
+    $Strict,
 
-  # Searches the command from the leaf of the path specified by -Name and returns the path if found. If not found, searches all locations by using es.exe.
-  [switch]
-  $Program
+    # Searches the command from the leaf of the path specified by -Name and returns the path if found. If not found, searches all locations by using es.exe.
+    [switch]
+    $Program
 )
 
 Set-StrictMode -Version 'Latest'
 
 # 結果を処理するスクリプト
 $isCompleated = {
-  param (
-    [string[]]
-    $AddPath
-  )
+    param (
+        [string[]]
+        $AddPath
+    )
 
-  if (!$AddPath) {
-    return $false
-  }
-
-  $resultPaths.AddRange($AddPath)
-  $resultItems = Get-Item -LiteralPath $resultPaths -ErrorAction Ignore
-
-  if ($Exclude) {
-    foreach ($aExclude in $Exclude) {
-      $resultItems = $resultItems | Where-Object { !(Select-String -InputObject $_.FullName -SimpleMatch $aExclude) }
-    }
-  }
-
-  $resultItems = $patterns | ForEach-Object {
-    $pattern = $_
-
-    $completedItem = $resultItems |
-    Where-Object {
-      if (!$pattern.Parent) {
-        return $true
-      }
-      return Select-String -InputObject (Split-Path $_ -Parent) -SimpleMatch $pattern.Parent
-    } |
-    Where-Object {
-      if ($Strict) {
-        return $_.Name -eq $pattern.Leaf
-      }
-      return Select-String -InputObject $_.Name -SimpleMatch $pattern.Leaf
+    if (!$AddPath) {
+        return $false
     }
 
-    if ($completedItem) {
-      $completedItem
-      $completedLeafs.add($pattern.Leaf) | Out-Null
+    $resultPaths.AddRange($AddPath)
+    $resultItems = Get-Item -LiteralPath $resultPaths -ErrorAction Ignore
+
+    if ($Exclude) {
+        foreach ($aExclude in $Exclude) {
+            $resultItems = $resultItems | Where-Object { !$_.FullName.Contains($aExclude) }
+        }
     }
-  }
 
-  $resultPaths.Clear()
-  if (!$resultItems) {
-    return $false
-  }
-  $resultPaths.AddRange([string[]](Convert-Path -LiteralPath $resultItems.FullName | Select-Object -Unique))
+    $resultItems = $patterns | ForEach-Object {
+        $pattern = $_
 
-  # $leafsを未取得のもので上書き
-  [string[]]$leafs = $leafs | Where-Object { $completedLeafs -notcontains $_ }
+        $completedItem = $resultItems |
+        Where-Object {
+            if (!$pattern.Parent) {
+                return $true
+            }
+            return Select-String -InputObject (Split-Path $_ -Parent) -SimpleMatch $pattern.Parent
+        } |
+        Where-Object {
+            if ($Strict) {
+                return $_.Name -eq $pattern.Leaf
+            }
+            return Select-String -InputObject $_.Name -SimpleMatch $pattern.Leaf
+        }
 
-  $completedLeafs.Clear()
+        if ($completedItem) {
+            $completedItem
+            $completedLeafs.add($pattern.Leaf) | Out-Null
+        }
+    }
 
-  return !$leafs
+    $resultPaths.Clear()
+    if (!$resultItems) {
+        return $false
+    }
+    $resultPaths.AddRange([string[]](Convert-Path -LiteralPath $resultItems.FullName | Select-Object -Unique))
+
+    # $leafsを未取得のもので上書き
+    [string[]]$leafs = $leafs | Where-Object { $completedLeafs -notcontains $_ }
+
+    $completedLeafs.Clear()
+
+    return !$leafs
 }
 
 # パスセパレータを\で統一
 $Name = $Name -replace '/', '\'
 
 $patterns = $Name | ForEach-Object {
-  @{
-    Leaf   = Split-Path $_ -Leaf
-    Parent = Split-Path $_ -Parent
-  }
+    @{
+        Leaf   = Split-Path $_ -Leaf
+        Parent = Split-Path $_ -Parent
+    }
 }
 
 $leafs = $patterns.Leaf
 $resultPaths = New-Object System.Collections.ArrayList
 $completedLeafs = New-Object System.Collections.ArrayList
 
+$Exclude += @(
+    'C:\Windows\SysWOW64'
+    'SxS'
+    "$env:LOCALAPPDATA\Microsoft\Windows\FileHistory"
+)
+
 if ($Program) {
-  # コマンドから探す
-  [string[]]$cmdPaths = Get-Command $leafs -ErrorAction Ignore | Select-Object -ExpandProperty Path
+    # コマンドから探す
+    [string[]]$cmdPaths = Get-Command $leafs -ErrorAction Ignore | Select-Object -ExpandProperty Path
 
-  if ($cmdPaths) {
-    $cmdResultPaths = @()
-    [string[]]$scoopShimPaths = $cmdPaths |
-    Where-Object {
-      (Split-Path $_ -Parent) -like '*scoop\shims' -and
-      (Split-Path $_ -Leaf) -match '\.exe$'
-    }
-    $GeneralCmdPaths = $cmdPaths | Where-Object { $scoopShimPaths -notcontains $_ }
-
-    $cmdResultPaths += $GeneralCmdPaths
-
-    if ($scoopShimPaths) {
-      if ((Get-Command -Name scoop -ErrorAction Ignore)) {
-        $scoopCmdPaths = Split-Path $scoopShimPaths -Leaf | ForEach-Object {
-          scoop which ($_ -replace '\.exe$', '')
+    if ($cmdPaths) {
+        $cmdResultPaths = @()
+        [string[]]$scoopShimPaths = $cmdPaths |
+        Where-Object {
+            (Split-Path $_ -Parent) -like '*scoop\shims' -and
+            (Split-Path $_ -Leaf) -match '\.exe$'
         }
-        $cmdResultPaths += $scoopCmdPaths
-      }
-      else {
-        $cmdResultPaths += $scoopShimPaths
-      }
+        $GeneralCmdPaths = $cmdPaths | Where-Object { $scoopShimPaths -notcontains $_ }
+
+        $cmdResultPaths += $GeneralCmdPaths
+
+        if ($scoopShimPaths) {
+            if ((Get-Command -Name scoop -ErrorAction Ignore)) {
+                $scoopCmdPaths = Split-Path $scoopShimPaths -Leaf | ForEach-Object {
+                    scoop which ($_ -replace '\.exe$', '')
+                }
+                $cmdResultPaths += $scoopCmdPaths
+            }
+            else {
+                $cmdResultPaths += $scoopShimPaths
+            }
+        }
+
+        if ((& $isCompleated -AddPath $cmdResultPaths)) {
+            return $resultPaths
+        }
     }
 
-    if ((& $isCompleated -AddPath $cmdResultPaths)) {
-      return $resultPaths
+    # ショートカットから探す
+    $lnkDirs = @(
+        "$env:APPDATA\Microsoft\Windows\Start Menu"
+        "C:\ProgramData\Microsoft\Windows\Start Menu"
+    )
+
+    $lnkResultPaths = Get-WULnkTarget -LiteralPath (Get-ChildItem -LiteralPath $lnkDirs -Recurse | Where-Object { $_.Extension -eq '.lnk' } | Select-Object -ExpandProperty FullName) -WarningAction Ignore
+
+    if ((& $isCompleated -AddPath $lnkResultPaths)) {
+        return $resultPaths
     }
-  }
-
-  # ショートカットから探す
-  $lnkDirs = @(
-    "$env:APPDATA\Microsoft\Windows\Start Menu"
-    "C:\ProgramData\Microsoft\Windows\Start Menu"
-  )
-
-  $lnkResultPaths = Get-WULnkTarget -LiteralPath (Get-ChildItem -LiteralPath $lnkDirs -Recurse | Where-Object { $_.Extension -eq '.lnk' } | Select-Object -ExpandProperty FullName) -WarningAction Ignore
-
-  if ((& $isCompleated -AddPath $lnkResultPaths)) {
-    return $resultPaths
-  }
 }
 
 # es.exeで探す
 [string[]]$esResultPaths = $leafs | ForEach-Object {
-  es.exe $_
+    es.exe $_
 }
 
 if ((& $isCompleated -AddPath $esResultPaths)) {
-  return $resultPaths
+    return $resultPaths
 }
 
 return $resultPaths
