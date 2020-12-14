ee14882ea3241ff6dfa27fe46c7bff585ec0ebfd
yuusakuri <yuusakurip1@gmail.com>
2020-12-14 12:49:38 +0900

    タスクバーの検索ボックスの表示モードを設定するスクリプトを追加

---
+++ functions/registry/Set-WUSearchBoxTaskbarMode.ps1
@@ -0,0 +1,19 @@
+﻿<#
+  .DESCRIPTION
+  This cmdlet works with registry.
+#>
+
+[CmdletBinding(SupportsShouldProcess)]
+param (
+  [string]
+  [ValidateSet('Box', 'Icon', 'None')]
+  $Mode
+)
+
+Set-StrictMode -Version 'Latest'
+$registryHash = Get-WURegistryHash
+if (!$registryHash) {
+  return
+}
+
+Set-WURegistryFromHash -RegistryHash $registryHash -DataKey $Mode
---
+++ resources/registry/SearchBoxTaskbarMode.ps1
@@ -0,0 +1,14 @@
+﻿@{
+    SearchBoxTaskbarMode = @{
+        CurrentUser = @{
+            KeyName   = 'HKCU\Software\Microsoft\Windows\CurrentVersion\Search'
+            ValueName = 'SearchboxTaskbarMode'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Box  = 2
+                Icon = 1
+                None = 0
+            }
+        }
+    }
+}
