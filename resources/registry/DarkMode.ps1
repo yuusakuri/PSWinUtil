c38dbd1a055e3c68ef5117651f55d464e1783257
yuusakuri <yuusakurip1@gmail.com>
2020-12-14 12:39:52 +0900

    Windowsにダークモードを設定するためのスクリプトを追加

---
+++ resources/registry/DarkMode.ps1
@@ -0,0 +1,24 @@
+﻿@{
+    SystemUsesDarkTheme = @{
+        CurrentUser = @{
+            KeyName   = 'HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize'
+            ValueName = 'SystemUsesLightTheme'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Enable  = 0
+                Disable = 1
+            }
+        }
+    }
+    AppsUseDarkTheme    = @{
+        CurrentUser = @{
+            KeyName   = 'HKCU\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize'
+            ValueName = 'AppsUseLightTheme'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Enable  = 0
+                Disable = 1
+            }
+        }
+    }
+}
