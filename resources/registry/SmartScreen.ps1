b566eb547f9bf746823f995e2cb31b648fd05ce0
yuusakuri <yuusakurip1@gmail.com>
2020-12-14 12:42:08 +0900

    Windows Defender Smart Screenも設定するように変更

--- resources/registry/SmartScreen.ps1
+++ resources/registry/SmartScreen.ps1
@@ -1,14 +1,24 @@
 ﻿@{
-  SmartScreen = @{
-    LocalMachine = @{
-      KeyName   = 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System
-      '
-      ValueName = 'EnableSmartScreen'
-      Type      = 'REG_DWORD'
-      Data      = @{
-        Enable  = 1
-        Disable = 0
-      }
+    WindowsSmartScreen         = @{
+        LocalMachine = @{
+            KeyName   = 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System'
+            ValueName = 'EnableSmartScreen'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Enable  = 1
+                Disable = 0
+            }
+        }
     }
-  }
+    WindowsDefenderSmartScreen = @{
+        LocalMachine = @{
+            KeyName   = 'HKLM\SOFTWARE\Policies\Microsoft\MicrosoftEdge\PhishingFilter'
+            ValueName = 'EnabledV9'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Enable  = 1
+                Disable = 0
+            }
+        }
+    }
 }
