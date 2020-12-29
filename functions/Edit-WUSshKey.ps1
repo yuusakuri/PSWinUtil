79c3f5dc2eb0b273a415f38a3e917b15f536807f
yuusakuri <yuusakurip1@gmail.com>
2020-12-29 16:31:48 +0900

    最低文字数を修正

--- functions/Edit-WUSshKey.ps1
+++ functions/Edit-WUSshKey.ps1
@@ -26,73 +26,73 @@
 
 [CmdletBinding(SupportsShouldProcess)]
 param (
-  # Specify the location of the key file to be created, relative path from '~/.ssh' or absolute path.
-  [Parameter(Mandatory,
-    Position = 0,
-    ValueFromPipeline,
-    ValueFromPipelineByPropertyName)]
-  [ValidateNotNullOrEmpty()]
-  [string]
-  $Path,
+    # Specify the location of the key file to be created, relative path from '~/.ssh' or absolute path.
+    [Parameter(Mandatory,
+        Position = 0,
+        ValueFromPipeline,
+        ValueFromPipelineByPropertyName)]
+    [ValidateNotNullOrEmpty()]
+    [string]
+    $Path,
 
-  # Specify a new passphrase.
-  [string]
-  $NewPassphrase,
+    # Specify a new passphrase.
+    [string]
+    $NewPassphrase,
 
-  # Specify the current passphrase.
-  [string]
-  $CurrentPassphrase,
+    # Specify the current passphrase.
+    [string]
+    $CurrentPassphrase,
 
-  # Specify a new comment.
-  [string]
-  $Comment
+    # Specify a new comment.
+    [string]
+    $Comment
 )
 
 Set-StrictMode -Version 'Latest'
 
 # コマンドの引数に空文字を渡す場合にエスケープさせる
 $emptyParam = @{
-  ''   = """"""
-  '''' = """"""
-  """" = """"""
+    ''   = """"""
+    '''' = """"""
+    """" = """"""
 }
 if ($emptyParam.ContainsKey($Comment)) {
-  $Comment = $emptyParam.$Comment
+    $Comment = $emptyParam.$Comment
 }
 if ($emptyParam.ContainsKey($CurrentPassphrase)) {
-  $CurrentPassphrase = $emptyParam.$CurrentPassphrase
+    $CurrentPassphrase = $emptyParam.$CurrentPassphrase
 }
 if ($emptyParam.ContainsKey($NewPassphrase)) {
-  $NewPassphrase = $emptyParam.$NewPassphrase
+    $NewPassphrase = $emptyParam.$NewPassphrase
 }
-elseif ($NewPassphrase.Length -le 5) {
-  Write-Error 'Passphrase must be a minimum of 5 characters.'
-  return
+elseif ($NewPassphrase.Length -lt 5) {
+    Write-Error 'Passphrase must be a minimum of 5 characters.'
+    return
 }
 
 $keyPath = Resolve-WUFullPath -LiteralPath $Path -BasePath '~/.ssh'
 
 if (!(Test-Path -LiteralPath $keyPath)) {
-  Write-Error "Cannot find path '$keyPath' because it does not exist." -Category ObjectNotFound
-  return
+    Write-Error "Cannot find path '$keyPath' because it does not exist." -Category ObjectNotFound
+    return
 }
 
 if ($PSBoundParameters.ContainsKey('Comment')) {
-  $resultMess = ssh-keygen -qo -c -C "$Comment" -P "$CurrentPassphrase" -f "$keyPath"
+    $resultMess = ssh-keygen -qo -c -C "$Comment" -P "$CurrentPassphrase" -f "$keyPath"
 
-  if (!$resultMess -or !$resultMess.Contains('The comment in your key file has been changed.')) {
-    Write-Error 'Failed to change the comment.'
-    return
-  }
+    if (!$resultMess -or !$resultMess.Contains('The comment in your key file has been changed.')) {
+        Write-Error 'Failed to change the comment.'
+        return
+    }
 }
 
 if ($PSBoundParameters.ContainsKey('NewPassphrase')) {
-  $resultMess = ssh-keygen -qo -p -P "$CurrentPassphrase" -N "$NewPassphrase" -f "$keyPath"
+    $resultMess = ssh-keygen -qo -p -P "$CurrentPassphrase" -N "$NewPassphrase" -f "$keyPath"
 
-  if (!$resultMess -or !$resultMess.Contains('Your identification has been saved with the new passphrase.')) {
-    Write-Error 'Failed to change the Passphrase.'
-    return
-  }
+    if (!$resultMess -or !$resultMess.Contains('Your identification has been saved with the new passphrase.')) {
+        Write-Error 'Failed to change the Passphrase.'
+        return
+    }
 }
 
 return $keyPath
