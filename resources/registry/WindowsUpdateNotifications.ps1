55bba5aa2bb0e5e9504f6687693e4546b04e1602
yuusakuri <yuusakurip1@gmail.com>
2021-01-23 01:35:42 +0900

    Add registry data to completely disable Windows Update notifications

--- resources/registry/WindowsUpdateNotifications.ps1
+++ resources/registry/WindowsUpdateNotifications.ps1
@@ -1,5 +1,5 @@
 ﻿@{
-    WindowsUpdateNotifications = @{
+    SetAutoRestartNotificationDisable = @{
         LocalMachine = @{
             KeyName   = 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\WindowsUpdate'
             ValueName = 'SetAutoRestartNotificationDisable'
@@ -7,6 +7,28 @@
             Data      = @{
                 Enable  = 0
                 Disable = 1
+            }
+        }
+    }
+    SetUpdateNotificationLevel        = @{
+        LocalMachine = @{
+            KeyName   = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate'
+            ValueName = 'SetUpdateNotificationLevel'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Enable  = 1
+                Disable = 1
+            }
+        }
+    }
+    UpdateNotificationLevel           = @{
+        LocalMachine = @{
+            KeyName   = 'HKEY_LOCAL_MACHINE\Software\Policies\Microsoft\Windows\WindowsUpdate'
+            ValueName = 'UpdateNotificationLevel'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Enable  = 0
+                Disable = 2
             }
         }
     }
