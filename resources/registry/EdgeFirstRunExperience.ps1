328f8df5cc6ccd7be35370fc5eeb1e4bde7d9035
yuusakuri <yuusakurip1@gmail.com>
2021-01-31 06:16:28 +0900

    Add functions to enable or disable Microsoft Edge first run experience and splash screen

---
+++ resources/registry/EdgeFirstRunExperience.ps1
@@ -0,0 +1,13 @@
+﻿@{
+    EdgeFirstRunExperience = @{
+        LocalMachine = @{
+            KeyName   = 'HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Edge'
+            ValueName = 'HideFirstRunExperience'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Enable  = 0
+                Disable = 1
+            }
+        }
+    }
+}
