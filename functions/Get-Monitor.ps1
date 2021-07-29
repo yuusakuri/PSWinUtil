f2ed6ca92d6655f6874235d7ec585fb5d5869138
yuusakuri <yuusakurip1@gmail.com>
2021-07-29 21:01:58 +0900

    Add and modify the properties of the output object

--- functions/Get-Monitor.ps1
+++ functions/Get-Monitor.ps1
@@ -5,9 +5,6 @@
 
         .DESCRIPTION
         Get the following monitor information:
-            deviceName
-            monitorHandle
-            displayModes
             resolution
             verticalResolution
             horizontalResolution
@@ -22,6 +19,7 @@
             maximumResolution
             maximumVerticalResolution
             maximumHorizontalResolution
+            deviceName
             monitorIndex
             adapter
             deviceId
@@ -31,6 +29,23 @@
             monitorString
             monitorName
             monitorSerialNumber
+            pnpDeviceId
+            manufactureWeek
+            manufacturerId
+            productId
+            numericSerialNumber
+            edidVersion
+            displayGamma
+            verticalFrequency
+            horizontalFrequency
+            imageSize
+            maximumImageSize
+            supportStandbyMode
+            supportSuspendMode
+            supportLowPowerMode
+            supportDefaultGtf
+            digital
+            supportedDisplayModes
 
         .OUTPUTS
         System.Management.Automation.PSCustomObject
@@ -114,14 +129,15 @@
 
             Start-Process 'MultiMonitorTool.exe' -ArgumentList ('/sxml "{0}"' -f ($tempFilePath | Convert-WUString -Type EscapeForPowerShellDoubleQuotation)) -Wait -NoNewWindow -WhatIf:$false
 
-            try {
-                $monitorXml = Select-Xml -LiteralPath $tempFilePath -XPath 'monitors_list/item' -ErrorAction Stop | Select-Object -ExpandProperty Node
-            }
-            catch {
-                Write-Verbose $_
-                Write-Error "Failed to get monitor information from 'MultiMonitorTool'." -ErrorAction $ErrorActionPreference
+            $monitorXml = $null
+            $monitorXml = Select-Xml -LiteralPath $tempFilePath -XPath 'monitors_list/item' | Select-Object -ExpandProperty Node
+
+            if (!$monitorXml) {
+                Write-Error "Failed to get monitor information from 'MultiMonitorTool'."
                 return
             }
+
+            Write-Debug ($monitorXml | Format-Table | Out-String)
 
             foreach ($aMonitorXml in $monitorXml) {
                 $monitor = New-Object -TypeName psobject
@@ -149,111 +165,181 @@
         }
     }
 
-    function Get-WUMonitorFromSharpDX {
+    function Get-WUMonitorFromDumpEdid {
         <#
             .SYNOPSIS
-            Get Monitor details from SharpDX.
+            Get Monitor details from MonitorTool.
 
             .DESCRIPTION
             Get the following monitor information:
-                deviceName
-                monitorHandle
-                displayModes
+                active
+                registryKey
+                monitorName
+                manufactureWeek
+                manufacturerID
+                productID
+                serialNumberNumeric
+                eDIDVersion
+                displayGamma
+                verticalFrequency
+                horizontalFrequency
+                imageSize
+                maximumImageSize
+                maximumResolution
+                supportStandbyMode
+                supportSuspendMode
+                supportLowPowerMode
+                supportDefaultGTF
+                digital
+                supportedDisplayModes
 
             .OUTPUTS
             System.Management.Automation.PSCustomObject
 
             .EXAMPLE
-            PS C:\>Get-WUMonitorFromSharpDX
+            PS C:\>Get-WUMonitorFromDumpEdid
         #>
-
         [CmdletBinding()]
         param (
         )
 
-        # [How to get supported display modes using SharpDX](https://discussiongenerator.com/2012/11/04/how-to-get-supported-display-modes-using-sharpdx/)
+        $resultStrings = @()
+        $resultStrings += Invoke-Expression 'dumpedid'
+        if (!$resultStrings) {
+            return
+        }
 
-        Import-WUNuGetPackageAssembly -Install -PackageID SharpDX.DXGI -WhatIf:$false
+        Write-Debug ('dumpedid:', $resultStrings | Format-Table | Out-String)
 
-        $dxgiFactory = New-Object 'SharpDX.DXGI.Factory1'
+        (
+            ($resultStrings | Where-Object { ![String]::IsNullOrWhiteSpace($_) }) -join [System.Environment]::NewLine
+        ) -split '[*]{65}' |
+        Where-Object { ![String]::IsNullOrWhiteSpace($_) } |
+        Select-Object -Skip 1 |
+        ForEach-Object {
+            $aComputerString = $_
 
-        foreach ($dxgiAdapter in $dxgiFactory.Adapters) {
-            foreach ($aOutput in $dxgiAdapter.Outputs) {
-                [PSCustomObject]@{
-                    deviceName    = $aOutput.Description.DeviceName
-                    monitorHandle = $aOutput.Description.MonitorHandle
-                    displayModes  = [System.Enum]::GetValues([SharpDX.DXGI.Format]) |
-                    ForEach-Object {
-                        $aFormat = $_
+            $monitor = New-Object -TypeName psobject
+            $displayModes = @()
 
-                        $displayModes = $aOutput.GetDisplayModeList(
-                            [SharpDX.DXGI.Format]$aFormat,
-                            [SharpDX.DXGI.DisplayModeEnumerationFlags]::Interlaced -band [SharpDX.DXGI.DisplayModeEnumerationFlags]::Scaling
-                        )
+            $aComputerString -split [System.Environment]::NewLine |
+            Where-Object { ![String]::IsNullOrWhiteSpace($_) } |
+            ForEach-Object {
+                $aLine = $_
 
-                        foreach ($aDisplayMode in $displayModes) {
-                            if ($aDisplayMode.Scaling -eq [SharpDX.DXGI.DisplayModeScaling]::Unspecified) {
-                                [PSCustomObject]@{
-                                    horizontalResolution = $aDisplayMode.Width
-                                    verticalResolution   = $aDisplayMode.Height
-                                    frequency            = $aDisplayMode.RefreshRate
-                                }
-                            }
-                        }
+                if ($aLine -match '^\s*(?<horizontalResolution>\d+)\s*X\s*(?<verticalResolution>\d+)\s*(?<frequency>\d+)\s*Hz\s*$') {
+                    $displayModes += [PSCustomObject]@{
+                        horizontalResolution = [int]$Matches['horizontalResolution']
+                        verticalResolution   = [int]$Matches['verticalResolution']
+                        frequency            = [int]$Matches['frequency']
                     }
                 }
+                elseif ($aLine -match '^(?<name>Active|Registry Key|Monitor Name|Manufacture Week|ManufacturerID|ProductID|Serial Number \(Numeric\)|EDID Version|Display Gamma|Vertical Frequency|Horizontal Frequency|Image Size|Maximum Image Size|Maximum Resolution|Support Standby Mode|Support Suspend Mode|Support Low-Power Mode|Support Default GTF|Digital)\s*:\s*(?<value>.+)$') {
+                    $monitor | Add-Member -MemberType NoteProperty -Name (Convert-WUString -String $Matches['name'] -Type 'LowerCamelCase') -Value $Matches['value']
+                }
+                elseif ($aLine -match '^(?<name>Supported Display Modes)\s*:\s*$') {
+                }
+                else {
+                    Write-Debug "Line that is unsupported format: $aLine"
+                }
             }
+
+            $monitor | Add-Member -MemberType NoteProperty -Name (Convert-WUString -String 'Supported Display Modes' -Type 'LowerCamelCase') -Value $displayModes
+
+            $monitor
         }
     }
 
-    $monitorsFromMultiMonitorTool = Get-WUMonitorFromMultiMonitorTool
+    $monitorsFromMultiMonitorTool = @()
+    $monitorsFromMultiMonitorTool += Get-WUMonitorFromMultiMonitorTool
     if (!$monitorsFromMultiMonitorTool) {
         return
     }
-    $monitorsFromSharpDX = Get-WUMonitorFromSharpDX
-    if (!$monitorsFromSharpDX) {
+
+    $monitorsFromDumpEdid = @()
+    $monitorsFromDumpEdid += Get-WUMonitorFromDumpEdid
+    if (!$monitorsFromDumpEdid) {
         return
     }
 
-    foreach ($aMmonitorFromMultiMonitorTool in $monitorsFromMultiMonitorTool) {
-        $monitor = New-Object -TypeName psobject
-        $aMonitorFromSharpDX = $monitorsFromSharpDX | Where-Object { $_.deviceName -eq $aMmonitorFromMultiMonitorTool.name }
+    if ($monitorsFromMultiMonitorTool.Count -ne $monitorsFromDumpEdid.Count) {
+        Write-Error "The number of monitors detected does not match."
+        return
+    }
 
-        foreach ($aMonitorFromSharpDXProperty in $aMonitorFromSharpDX.psobject.Properties) {
-            $monitor | Add-Member -MemberType NoteProperty -Name $aMonitorFromSharpDXProperty.name -Value $aMonitorFromSharpDXProperty.Value
-        }
-        foreach ($aMmonitorFromMultiMonitorToolProperty in $aMmonitorFromMultiMonitorTool.psobject.Properties) {
-            switch ($aMmonitorFromMultiMonitorToolProperty.name) {
-                { $_ -in 'active', 'disconnected', 'primary' } {
-                    $monitor | Add-Member -MemberType NoteProperty -Name $aMmonitorFromMultiMonitorToolProperty.name -Value (Convert-StringToBool -String $aMmonitorFromMultiMonitorToolProperty.Value)
-                    break
-                }
-                'colors' {
-                    $monitor | Add-Member -MemberType NoteProperty -Name 'bitsPerPixel' -Value $aMmonitorFromMultiMonitorToolProperty.Value
-                    break
-                }
+    for ($i = 0; $i -lt $monitorsFromMultiMonitorTool.Count; $i++) {
+        $monitor = New-Object -TypeName psobject
+        $aMonitorFromMultiMonitorTool = $monitorsFromMultiMonitorTool[$i]
+        $aMonitorFromDumpEdid = $monitorsFromDumpEdid[$i]
+
+        foreach ($aMonitorFromMultiMonitorToolProperty in $aMonitorFromMultiMonitorTool.psobject.Properties) {
+            switch ($aMonitorFromMultiMonitorToolProperty.Name) {
                 'name' {
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'deviceName' -Value $aMonitorFromMultiMonitorToolProperty.Value
                     $monitorIndex = ''
-                    if ($aMmonitorFromMultiMonitorToolProperty.Value -match '(?<monitorIndex>\d+)') {
+                    if ($aMonitorFromMultiMonitorToolProperty.Value -match '(?<monitorIndex>\d+)') {
                         $monitorIndex = $Matches['monitorIndex']
                     }
                     $monitor | Add-Member -MemberType NoteProperty -Name 'monitorIndex' -Value $monitorIndex
                     break
                 }
                 'resolution' {
-                    $monitor | Add-Member -MemberType NoteProperty -Name $aMmonitorFromMultiMonitorToolProperty.name -Value $aMmonitorFromMultiMonitorToolProperty.Value
-                    $monitor | Add-Member -MemberType NoteProperty -Name 'verticalResolution' -Value ($aMmonitorFromMultiMonitorToolProperty.Value -replace '.+ X ')
-                    $monitor | Add-Member -MemberType NoteProperty -Name 'horizontalResolution' -Value ($aMmonitorFromMultiMonitorToolProperty.Value -replace ' X .+')
+                    $monitor | Add-Member -MemberType NoteProperty -Name $aMonitorFromMultiMonitorToolProperty.Name -Value $aMonitorFromMultiMonitorToolProperty.Value
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'verticalResolution' -Value ($aMonitorFromMultiMonitorToolProperty.Value -replace '.+ X ')
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'horizontalResolution' -Value ($aMonitorFromMultiMonitorToolProperty.Value -replace ' X .+')
                     break
                 }
                 'maximumResolution' {
-                    $monitor | Add-Member -MemberType NoteProperty -Name $aMmonitorFromMultiMonitorToolProperty.name -Value $aMmonitorFromMultiMonitorToolProperty.Value
-                    $monitor | Add-Member -MemberType NoteProperty -Name 'maximumVerticalResolution' -Value ($aMmonitorFromMultiMonitorToolProperty.Value -replace '.+ X ')
-                    $monitor | Add-Member -MemberType NoteProperty -Name 'maximumHorizontalResolution' -Value ($aMmonitorFromMultiMonitorToolProperty.Value -replace ' X .+')
+                    $monitor | Add-Member -MemberType NoteProperty -Name $aMonitorFromMultiMonitorToolProperty.Name -Value $aMonitorFromMultiMonitorToolProperty.Value
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'maximumVerticalResolution' -Value ($aMonitorFromMultiMonitorToolProperty.Value -replace '.+ X ')
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'maximumHorizontalResolution' -Value ($aMonitorFromMultiMonitorToolProperty.Value -replace ' X .+')
+                    break
+                }
+                'colors' {
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'bitsPerPixel' -Value $aMonitorFromMultiMonitorToolProperty.Value
+                    break
+                }
+                { $_ -in 'active', 'disconnected', 'primary' } {
+                    $monitor | Add-Member -MemberType NoteProperty -Name $aMonitorFromMultiMonitorToolProperty.Name -Value (Convert-StringToBool -String $aMonitorFromMultiMonitorToolProperty.Value)
                     break
                 }
                 Default {
-                    $monitor | Add-Member -MemberType NoteProperty -Name $aMmonitorFromMultiMonitorToolProperty.name -Value $aMmonitorFromMultiMonitorToolProperty.Value
+                    $monitor | Add-Member -MemberType NoteProperty -Name $aMonitorFromMultiMonitorToolProperty.Name -Value $aMonitorFromMultiMonitorToolProperty.Value
+                }
+            }
+        }
+        foreach ($aMonitorFromDumpEdidProperty in $aMonitorFromDumpEdid.psobject.Properties) {
+            switch ($aMonitorFromDumpEdidProperty.Name) {
+                'registryKey' {
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'pnpDeviceId' -Value $aMonitorFromDumpEdidProperty.Value
+                    break
+                }
+                { $_ -in 'manufactureWeek', 'displayGamma', 'verticalFrequency', 'horizontalFrequency', 'imageSize', 'maximumImageSize', 'supportedDisplayModes' } {
+                    $monitor | Add-Member -MemberType NoteProperty -Name $aMonitorFromDumpEdidProperty.Name -Value $aMonitorFromDumpEdidProperty.Value
+                    break
+                }
+                'manufacturerID' {
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'manufacturerId' -Value $aMonitorFromDumpEdidProperty.Value
+                    break
+                }
+                'productID' {
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'productId' -Value $aMonitorFromDumpEdidProperty.Value
+                }
+                'serialNumberNumeric' {
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'numericSerialNumber' -Value $aMonitorFromDumpEdidProperty.Value
+                    break
+                }
+                'eDIDVersion' {
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'edidVersion' -Value $aMonitorFromDumpEdidProperty.Value
+                    break
+                }
+                'supportDefaultGTF' {
+                    $monitor | Add-Member -MemberType NoteProperty -Name 'supportDefaultGtf' -Value (Convert-StringToBool -String $aMonitorFromDumpEdidProperty.Value)
+                    break
+                }
+                { $_ -in 'supportStandbyMode', 'supportSuspendMode', 'supportLowPowerMode', 'digital' } {
+                    $monitor | Add-Member -MemberType NoteProperty -Name $aMonitorFromDumpEdidProperty.Name -Value (Convert-StringToBool -String $aMonitorFromDumpEdidProperty.Value)
+                    break
                 }
             }
         }
