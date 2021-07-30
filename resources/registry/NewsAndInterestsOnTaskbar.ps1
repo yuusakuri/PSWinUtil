169da9b16b12c56808cbf2075159dd820eeff0d8
yuusakuri <yuusakurip1@gmail.com>
2021-07-30 10:14:07 +0900

    Add functions to enable or disable News and Interests on Taskbar

---
+++ resources/registry/NewsAndInterestsOnTaskbar.ps1
@@ -0,0 +1,13 @@
+﻿@{
+    NewsAndInterestsOnTaskbar = @{
+        LocalMachine = @{
+            Keyname   = 'HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\Windows Feeds'
+            Valuename = 'EnableFeeds'
+            Type      = 'REG_DWORD'
+            Data      = @{
+                Enable  = 1
+                Disable = 0
+            }
+        }
+    }
+}
