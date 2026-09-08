9d16294561a1d0a567dbe5ab6f6c0b4f94aebd3d
yuusakuri <yuusakurip1@gmail.com>
2026-09-08 11:12:09 +0900

    test: simplify module contract checks

--- tests/contract/ModuleManifest.Tests.ps1
+++ tests/contract/ModuleManifest.Tests.ps1
@@ -1,6 +1,9 @@
 BeforeAll {
     $repositoryRoot = Split-Path -Path (Split-Path -Path $PSScriptRoot -Parent) -Parent
     $script:ManifestPath = Join-Path -Path $repositoryRoot -ChildPath 'output/PSWinUtil/PSWinUtil.psd1'
+    $script:Manifest = Import-PowerShellDataFile -Path $script:ManifestPath
+    Import-Module -Name $script:ManifestPath -Force -ErrorAction Stop
+    $script:Module = Get-Module -Name 'PSWinUtil' -ErrorAction Stop
 }
 
 Describe 'Built module manifest' {
@@ -9,141 +12,16 @@ Describe 'Built module manifest' {
     }
 
     It 'targets Windows PowerShell 5.1 Desktop' {
-        $manifest = Import-PowerShellDataFile -Path $script:ManifestPath
-
-        $manifest.PowerShellVersion | Should -Be '5.1'
-        $manifest.CompatiblePSEditions | Should -Contain 'Desktop'
+        $script:Manifest.PowerShellVersion | Should -Be '5.1'
+        $script:Manifest.CompatiblePSEditions | Should -Contain 'Desktop'
     }
 
     It 'has no runtime module dependency' {
-        $manifest = Import-PowerShellDataFile -Path $script:ManifestPath
-
-        @($manifest.RequiredModules).Count | Should -Be 0
-    }
-
-    It 'exports every implemented setup command' {
-        Import-Module -Name $script:ManifestPath -Force -ErrorAction Stop
-        $exportedCommands = @(
-            (Get-Module -Name 'PSWinUtil' -ErrorAction Stop).ExportedFunctions.Keys
-        )
-        $expectedCommands = @(
-            'Get-WURegistryProperty'
-            'Set-WURegistryProperty'
-            'Remove-WURegistryProperty'
-            'Get-WURegistrySetting'
-            'Select-WUBoundParameter'
-            'Resolve-WUPath'
-            'Resolve-WUPathFromParameterSet'
-            'ConvertTo-WUNativeCommandArgument'
-            'ConvertTo-WUPSStringLiteral'
-            'Set-WUAdvertisingIdMode'
-            'Enable-WUAppLaunchTracking'
-            'Disable-WUAppLaunchTracking'
-            'Enable-WUAppSuggestions'
-            'Disable-WUAppSuggestions'
-            'Enable-WUDarkMode'
-            'Disable-WUDarkMode'
-            'Enable-WUEdgeFirstRunExperience'
-            'Disable-WUEdgeFirstRunExperience'
-            'Enable-WUFileHistory'
-            'Disable-WUFileHistory'
-            'Enable-WULockScreen'
-            'Disable-WULockScreen'
-            'Enable-WULockWorkstation'
-            'Disable-WULockWorkstation'
-            'Enable-WULongPaths'
-            'Disable-WULongPaths'
-            'Enable-WUWidgets'
-            'Disable-WUWidgets'
-            'Enable-WURequireSignInOnWakeup'
-            'Disable-WURequireSignInOnWakeup'
-            'Enable-WUSaveZoneInformation'
-            'Disable-WUSaveZoneInformation'
-            'Enable-WUSmartScreenInShell'
-            'Disable-WUSmartScreenInShell'
-            'Enable-WUUac'
-            'Disable-WUUac'
-            'Enable-WUWebsiteAccessToLanguageList'
-            'Disable-WUWebsiteAccessToLanguageList'
-            'Enable-WUWindowsHelloForBusiness'
-            'Disable-WUWindowsHelloForBusiness'
-            'Enable-WUWindowsMediaPlayerFirstUseDialogBoxes'
-            'Disable-WUWindowsMediaPlayerFirstUseDialogBoxes'
-            'Enable-WUWindowsSecurityAllNotifications'
-            'Disable-WUWindowsSecurityAllNotifications'
-            'Enable-WUWindowsSecurityNonCriticalNotifications'
-            'Disable-WUWindowsSecurityNonCriticalNotifications'
-            'Set-WUWindowsUpdateNotificationLevel'
-            'Get-WUStartupEntry'
-            'Register-WUStartupEntry'
-            'Unregister-WUStartupEntry'
-            'Get-WUWindowsAutoLogon'
-            'Enable-WUWindowsAutoLogon'
-            'Disable-WUWindowsAutoLogon'
-            'Get-WUKeyboardRemapping'
-            'Set-WUKeyboardRemapping'
-            'Remove-WUKeyboardRemapping'
-            'Set-WUNativeCommandEncoding'
-            'Set-WUNodeExtraCaCertificate'
-            'Set-WUJavaWindowsRootTrustStore'
-            'Start-WUAndroidEmulator'
-            'Get-WUAndroidEmulator'
-            'Get-WUAndroidEmulatorPort'
-            'Test-WUAndroidEmulatorPort'
-            'Get-WUAndroidCommandLineToolsUrl'
-            'Get-WUFlutterSdkUrl'
-            'Invoke-WUDefaultBrowserDownload'
-            'Install-WUAndroidCommandLineTools'
-            'Install-WUFlutterSdk'
-            'Install-WUGit'
-            'Install-WUGitHubCli'
-            'Install-WUWingetPackage'
-            'Set-WUJapaneseKeyboardLayout'
-            'Enable-WUDeviceSetupSuggestions'
-            'Disable-WUDeviceSetupSuggestions'
-            'Set-WUTaskbarAlignment'
-            'Set-WUTaskbarSearchMode'
-            'Enable-WUClassicContextMenu'
-            'Disable-WUClassicContextMenu'
-            'Set-WUJapaneseImeHalfWidthInput'
-            'Get-WUFileTreeWithContent'
-        )
-
-        if ($PSVersionTable.PSEdition -eq 'Desktop') {
-            $expectedCommands += @(
-                'Get-Content'
-                'Set-Content'
-                'Add-Content'
-                'Out-File'
-                'Invoke-WebRequest'
-            )
-        }
-
-        foreach ($expectedCommand in $expectedCommands) {
-            $exportedCommands | Should -Contain $expectedCommand
-        }
-    }
-
-    It 'keeps emulator discovery and monitoring helpers private' {
-        Import-Module -Name $script:ManifestPath -Force -ErrorAction Stop
-        $exportedCommands = @((Get-Module -Name 'PSWinUtil').ExportedFunctions.Keys)
-
-        foreach ($privateCommand in @(
-                'Test-WUTcpPort'
-                'Get-WUAndroidEmulatorUnavailablePort'
-                'ConvertTo-WUWindowsCommandLineArgument'
-                'Select-WUAndroidEmulatorPort'
-                'Wait-WUAndroidEmulator'
-            )) {
-            $exportedCommands | Should -Not -Contain $privateCommand
-        }
+        @($script:Manifest.RequiredModules).Count | Should -Be 0
     }
 
     It 'exports content command overrides only in Windows PowerShell' {
-        Import-Module -Name $script:ManifestPath -Force -ErrorAction Stop
-        $exportedCommands = @(
-            (Get-Module -Name 'PSWinUtil' -ErrorAction Stop).ExportedFunctions.Keys
-        )
+        $exportedCommands = @($script:Module.ExportedFunctions.Keys)
 
         foreach ($commandName in @(
                 'Get-Content'
@@ -162,70 +40,32 @@ Describe 'Built module manifest' {
     }
 
     It 'accepts multiple Scope values in every scoped public command' {
-        Import-Module -Name $script:ManifestPath -Force -ErrorAction Stop
-        $scopedCommandNames = @(
-            'Get-WUEnvironmentVariable'
-            'Set-WUEnvironmentVariable'
-            'Remove-WUEnvironmentVariable'
-            'Add-WUPathEnvironmentVariable'
-            'Remove-WUPathEnvironmentVariable'
-            'Install-WUGit'
-            'Get-WURegistrySetting'
-            'Get-WUStartupEntry'
-            'Register-WUStartupEntry'
-            'Unregister-WUStartupEntry'
-            'Set-WUNodeExtraCaCertificate'
-            'Set-WUJavaWindowsRootTrustStore'
+        $scopedCommands = @(
+            $script:Module.ExportedFunctions.Values |
+                Where-Object { $_.Parameters.ContainsKey('Scope') }
         )
 
-        foreach ($commandName in $scopedCommandNames) {
-            $command = Get-Command -Name $commandName -Module 'PSWinUtil'
-
+        foreach ($command in $scopedCommands) {
             $command.Parameters.Scope.ParameterType |
-                Should -Be ([string[]]) -Because "$commandName must accept multiple scopes"
+                Should -Be ([string[]]) -Because "$($command.Name) must accept multiple scopes"
         }
     }
 
     It 'uses consistent wildcard and literal parameters for file selectors' {
-        Import-Module -Name $script:ManifestPath -Force -ErrorAction Stop
-        $pathCommandTypes = @{
-            'Add-Content' = [string[]]
-            'Assert-WUPathProperty' = [string[]]
-            'Assert-WUPSScript' = [string[]]
-            'Edit-WUSshKey' = [string]
-            'Get-Content' = [string[]]
-            'Get-WUFileTreeWithContent' = [string[]]
-            'Resolve-WUPath' = [string[]]
-            'Resolve-WUPathFromParameterSet' = [string[]]
-            'Set-Content' = [string[]]
-            'Set-WUEnvironmentVariable' = [string[]]
-            'Set-WUNodeExtraCaCertificate' = [string]
-            'Start-WUPSScriptAsAdmin' = [string]
-            'Test-WUPathProperty' = [string[]]
-            'Test-WUPSScript' = [string[]]
-        }
-        $desktopOnlyCommandNames = @(
-            'Add-Content'
-            'Get-Content'
-            'Set-Content'
+        $fileSelectorCommands = @(
+            $script:Module.ExportedFunctions.Values |
+                Where-Object {
+                    $_.Parameters.ContainsKey('Path') -and
+                    $_.Parameters.Path.Attributes.TypeId -contains
+                    [System.Management.Automation.SupportsWildcardsAttribute]
+                }
         )
 
-        foreach ($commandName in $pathCommandTypes.Keys) {
-            $command = Get-Command `
-                -Name $commandName `
-                -Module 'PSWinUtil' `
-                -ErrorAction SilentlyContinue
-            if ($null -eq $command -and $commandName -in $desktopOnlyCommandNames) {
-                continue
-            }
-            $command | Should -Not -BeNullOrEmpty
-
+        foreach ($command in $fileSelectorCommands) {
             $command.Parameters.Keys | Should -Contain 'Path'
             $command.Parameters.Keys | Should -Contain 'LiteralPath'
             $command.Parameters.Path.ParameterType |
-                Should -Be $pathCommandTypes[$commandName]
-            $command.Parameters.LiteralPath.ParameterType |
-                Should -Be $pathCommandTypes[$commandName]
+                Should -Be $command.Parameters.LiteralPath.ParameterType
             $wildcardAttributes = @(
                 $command.Parameters.Path.Attributes |
                     Where-Object {
@@ -237,29 +77,9 @@ Describe 'Built module manifest' {
             $command.Parameters.LiteralPath.Aliases | Should -Contain 'LP'
         }
     }
-
-    It 'contains the public structured output type names' {
-        $modulePath = Join-Path -Path (Split-Path -Path $script:ManifestPath -Parent) -ChildPath 'PSWinUtil.psm1'
-        $moduleText = [System.IO.File]::ReadAllText($modulePath)
-
-        $moduleText | Should -Match "PSTypeName = 'PSWinUtil.RegistryProperty'"
-        $moduleText | Should -Match "PSTypeName = 'PSWinUtil.RegistrySetting'"
-        $moduleText | Should -Match "PSTypeName = 'PSWinUtil.StartupEntry'"
-        $moduleText | Should -Match "PSTypeName = 'PSWinUtil.WindowsAutoLogon'"
-        $moduleText | Should -Match "PSTypeName = 'PSWinUtil.KeyboardRemapping'"
-        $moduleText | Should -Match "PSTypeName = 'PSWinUtil.JapaneseKeyboardLayout'"
-        $moduleText | Should -Match "PSTypeName = 'PSWinUtil.FileTreeContent'"
-    }
 }
 
 Describe 'Public command help' {
-    BeforeAll {
-        Import-Module -Name $script:ManifestPath -Force -ErrorAction Stop
-        $script:PublicFunctionNames = @(
-            (Get-Module -Name 'PSWinUtil' -ErrorAction Stop).ExportedFunctions.Keys
-        )
-    }
-
     It 'is complete for every exported function' {
         $commonParameterNames = @(
             'Verbose'
@@ -277,7 +97,7 @@ Describe 'Public command help' {
             'WhatIf'
             'Confirm'
         )
-        foreach ($functionName in $script:PublicFunctionNames) {
+        foreach ($functionName in $script:Module.ExportedFunctions.Keys) {
             $help = Get-Help -Name "PSWinUtil\$functionName" -Full
 
             $help.Synopsis | Should -Not -BeNullOrEmpty
@@ -313,69 +133,6 @@ Describe 'Public command help' {
                         Where-Object { $_.Name -eq $parameterName }
                 )
                 $parameterHelp.Count | Should -Be 1 -Because "$functionName must document $parameterName"
-                $parameterHelp[0].Description | Should -Not -BeNullOrEmpty
-            }
-        }
-    }
-}
-
-Describe 'Private command help' {
-    BeforeAll {
-        Import-Module -Name $script:ManifestPath -Force -ErrorAction Stop
-        $script:Module = Get-Module -Name 'PSWinUtil' -ErrorAction Stop
-        $script:ExportedFunctionNames = @($script:Module.ExportedFunctions.Keys)
-        $script:PrivateFunctionNames = @(
-            & $script:Module {
-                Get-Command -CommandType Function |
-                    Where-Object { $_.ModuleName -eq 'PSWinUtil' } |
-                    Select-Object -ExpandProperty Name
-                } |
-                    Where-Object { $_ -notin $script:ExportedFunctionNames }
-        )
-    }
-
-    It 'is complete for every internal function' {
-        foreach ($functionName in $script:PrivateFunctionNames) {
-            $help = & $script:Module {
-                param($Name)
-
-                Get-Help -Name $Name -Full
-            } $functionName
-
-            $help.Synopsis | Should -Not -BeNullOrEmpty
-            @($help.Description).Count | Should -BeGreaterThan 0
-            @($help.Examples.Example).Count | Should -BeGreaterThan 0
-
-            $command = & $script:Module {
-                param($Name)
-
-                Get-Command -Name $Name -CommandType Function
-            } $functionName
-            foreach ($parameterName in $command.Parameters.Keys) {
-                if ($parameterName -in @(
-                        'Verbose'
-                        'Debug'
-                        'ErrorAction'
-                        'WarningAction'
-                        'InformationAction'
-                        'ErrorVariable'
-                        'WarningVariable'
-                        'InformationVariable'
-                        'OutVariable'
-                        'OutBuffer'
-                        'PipelineVariable'
-                        'ProgressAction'
-                        'WhatIf'
-                        'Confirm'
-                    )) {
-                    continue
-                }
-
-                $parameterHelp = @(
-                    $help.Parameters.Parameter |
-                        Where-Object { $_.Name -eq $parameterName }
-                )
-                $parameterHelp.Count | Should -Be 1
                 $parameterHelp[0].Description | Should -Not -BeNullOrEmpty
             }
         }
