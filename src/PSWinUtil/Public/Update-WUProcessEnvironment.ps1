6722054f4bc83f528bd253b02d9020c91b2f75bc
yuusakuri <yuusakurip1@gmail.com>
2026-09-21 04:00:15 +0900

    fix: ignore process-only variables in persistent PATH

--- src/PSWinUtil/Public/Update-WUProcessEnvironment.ps1
+++ src/PSWinUtil/Public/Update-WUProcessEnvironment.ps1
@@ -27,10 +27,6 @@ function Update-WUProcessEnvironment {
 
     $environmentValues = @{}
     $environmentLookup = @{}
-    foreach ($environmentEntry in [System.Environment]::GetEnvironmentVariables(
-            [System.EnvironmentVariableTarget]::Process).GetEnumerator()) {
-        $environmentLookup[[string]$environmentEntry.Key] = [string]$environmentEntry.Value
-    }
     foreach ($target in @(
             [System.EnvironmentVariableTarget]::Machine,
             [System.EnvironmentVariableTarget]::User
